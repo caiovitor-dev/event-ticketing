a4c3139e1f7b5a5527c4a877c67b90316ff64d25
Caio Vitor <caiocastro460@gmail.com>
2026-07-27 17:22:10 -0300

    feat: add Venue entity with migration

---
+++ src/main/java/dev/caiovitor/eventticketing/entity/Venue.java
@@ -0,0 +1,28 @@
+package dev.caiovitor.eventticketing.entity;
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.UUID;
+
+@Table(name = "venues")
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class Venue {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false,updatable = false)
+    private UUID id;
+
+    @Column(name = "name",nullable = false, length = 155)
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @Column(name = "total_capacity", nullable = false)
+    private Integer totalCapacity;
+}
