cc958d65b634000e2092f3d248cc07a567fdb3b6
Caio Vitor <caiocastro460@gmail.com>
2026-07-29 11:24:21 -0300

    fix: update EventSessionStatus enum and migration

--- src/main/java/dev/caiovitor/eventticketing/enums/EventSessionStatus.java
+++ src/main/java/dev/caiovitor/eventticketing/enums/EventSessionStatus.java
@@ -2,9 +2,7 @@ package dev.caiovitor.eventticketing.enums;
 
 public enum EventSessionStatus {
     ANNOUNCED,
-    OPEN,
-    SOLD_OUT,
-    CLOSED,
-    CANCELLED,
-
+    ACTIVE,
+    FINISHED,
+    CANCELLED
 }
