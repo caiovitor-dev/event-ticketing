d330eec37fc92588af55d2b1b6b9a628af32892b
Caio Vitor <caiocastro460@gmail.com>
2026-08-11 15:18:52 -0300

    fix: add DISTINCT to the user repository query

--- src/main/java/dev/caiovitor/eventticketing/repository/UserRepository.java
+++ src/main/java/dev/caiovitor/eventticketing/repository/UserRepository.java
@@ -5,13 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
 
-    @Query("SELECT u FROM User u LEFT JOIN FETCH u.roles WHERE u.email = :email")
+    @Query("SELECT DISTINCT u FROM User u LEFT JOIN FETCH u.roles WHERE u.email = :email")
     Optional<User> findByEmailWithRoles(@Param("email") String email);
 
 
