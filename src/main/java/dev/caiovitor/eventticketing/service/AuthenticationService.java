d8988536e8164c1a222a605b4c8e9cd964e8417f
Caio Vitor <caiocastro460@gmail.com>
2026-08-11 12:00:51 -0300

    feat: add AuthenticationService

--- src/main/java/dev/caiovitor/eventticketing/service/AuthenticationService.java
+++ src/main/java/dev/caiovitor/eventticketing/service/AuthenticationService.java
@@ -1,4 +1,33 @@
 package dev.caiovitor.eventticketing.service;
 
+import dev.caiovitor.eventticketing.dto.LoginDTO;
+import dev.caiovitor.eventticketing.dto.TokenResponseDTO;
+import dev.caiovitor.eventticketing.security.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+
+@RequiredArgsConstructor
+@Service
 public class AuthenticationService {
+
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+    
+    public TokenResponseDTO login(LoginDTO dto){
+
+        Authentication authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(dto.email(), dto.password()));
+
+        UserDetails userDetails = (UserDetails) authenticate.getPrincipal();
+
+        String token = jwtService.generateAccessToken(userDetails);
+
+        return new TokenResponseDTO(token);
+    }
+
 }
