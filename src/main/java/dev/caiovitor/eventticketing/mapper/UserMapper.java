b0f07d6c9db75d3b2f87ff03bd439ab09e2b6a9c
Caio Vitor <caiocastro460@gmail.com>
2026-08-11 12:23:29 -0300

    feat: add UserMapper

--- src/main/java/dev/caiovitor/eventticketing/mapper/UserMapper.java
+++ src/main/java/dev/caiovitor/eventticketing/mapper/UserMapper.java
@@ -3,9 +3,18 @@ package dev.caiovitor.eventticketing.mapper;
 import dev.caiovitor.eventticketing.dto.UserCreateDTO;
 import dev.caiovitor.eventticketing.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
-public interface AuthenticationMapper {
+public interface UserMapper {
 
+    @Mapping(source = "cep", target = "address.cep")
+    @Mapping(source = "street",target = "address.street")
+    @Mapping(source = "number",target = "address.number")
+    @Mapping(source = "complement",target = "address.complement")
+    @Mapping(source = "neighborhood",target = "address.neighborhood")
+    @Mapping(source = "city",target = "address.city")
+    @Mapping(source = "state",target = "address.state")
     public User toEntity(UserCreateDTO dto);
+
 }
