337c0d2ed6d6f9c3aa83fc86a1120958775159f4
Caio Vitor <caiocastro460@gmail.com>
2026-08-12 15:40:40 -0300

    feat: add GlobalExceptionHandler and handle role not found

--- src/main/java/dev/caiovitor/eventticketing/exception/RoleNotFoundException.java
+++ src/main/java/dev/caiovitor/eventticketing/exception/RoleNotFoundException.java
@@ -1,4 +1,7 @@
-package dev.caiovitor.eventticketing.exception.global;
+package dev.caiovitor.eventticketing.exception;
 
-public class RoleNotFoundException {
+public class RoleNotFoundException extends RuntimeException{
+    public RoleNotFoundException (String message){
+        super(message);
+    }
 }
--- src/main/java/dev/caiovitor/eventticketing/exception/global/GlobalExceptionHandler.java
+++ src/main/java/dev/caiovitor/eventticketing/exception/global/GlobalExceptionHandler.java
@@ -1,4 +1,24 @@
 package dev.caiovitor.eventticketing.exception.global;
 
+import dev.caiovitor.eventticketing.dto.ErrorResponseDTO;
+import dev.caiovitor.eventticketing.exception.RoleNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RoleNotFoundException.class)
+    public ResponseEntity<ErrorResponseDTO>  handlerRoleNotFound(RoleNotFoundException e){
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                new ErrorResponseDTO(
+                        HttpStatus.NOT_FOUND.value(),
+                        e.getMessage(),
+                        LocalDateTime.now()));
+    }
 }
