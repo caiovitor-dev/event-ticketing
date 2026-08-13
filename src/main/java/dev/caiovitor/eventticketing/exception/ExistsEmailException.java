a2d7f32fd357799620532d69cab4786637308467
Caio Vitor <caiocastro460@gmail.com>
2026-08-13 10:50:40 -0300

    refactor: remove unused classes

--- src/main/java/dev/caiovitor/eventticketing/exception/ExistsEmailException.java
+++
@@ -1,7 +0,0 @@
-package dev.caiovitor.eventticketing.exception;
-
-public class existsEmailException extends RuntimeException {
-  public existsEmailException(String message) {
-    super(message);
-  }
-}
--- src/main/java/dev/caiovitor/eventticketing/service/UserService.java
+++ src/main/java/dev/caiovitor/eventticketing/service/UserService.java
@@ -1,17 +1,10 @@
 package dev.caiovitor.eventticketing.service;
 
-import dev.caiovitor.eventticketing.entity.Role;
 import dev.caiovitor.eventticketing.entity.User;
-import dev.caiovitor.eventticketing.enums.RoleName;
-import dev.caiovitor.eventticketing.repository.RoleRepository;
 import dev.caiovitor.eventticketing.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
