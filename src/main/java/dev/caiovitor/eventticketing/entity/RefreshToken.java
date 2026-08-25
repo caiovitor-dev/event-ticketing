73ca69b02157daf4bd85a965c519ccfbd40fcf04
Caio Vitor <caiocastro460@gmail.com>
2026-08-25 19:34:29 -0300

    feat: add RefreshToken entity with migration

--- src/main/java/dev/caiovitor/eventticketing/entity/RefreshToken.java
+++ src/main/java/dev/caiovitor/eventticketing/entity/RefreshToken.java
@@ -1,4 +1,44 @@
 package dev.caiovitor.eventticketing.entity;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+@Entity
+@Table(name = "refresh_tokens")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
 public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "token", unique = true,  nullable = false,updatable = false)
+    private String token;
+
+    @Column(name = "expires_at", nullable = false)
+    private LocalDateTime expiresAt;
+
+    @CreatedDate
+    @Column(name = "created_at",nullable = false,updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "revoked_at")
+    private LocalDateTime revokedAt;
+
+
 }
