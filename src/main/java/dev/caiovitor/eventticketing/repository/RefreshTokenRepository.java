11c7c4fe707175b780d341c1df76dea64bd1324c
Caio Vitor <caiocastro460@gmail.com>
2026-08-28 15:48:18 -0300

    feat: add RefreshToken repository

--- src/main/java/dev/caiovitor/eventticketing/repository/RefreshTokenRepository.java
+++ src/main/java/dev/caiovitor/eventticketing/repository/RefreshTokenRepository.java
@@ -1,4 +1,9 @@
 package dev.caiovitor.eventticketing.repository;
 
-public interface RefreshTokenRepository {
+import dev.caiovitor.eventticketing.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
 }
