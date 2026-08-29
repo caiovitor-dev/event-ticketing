37201b7b5e8619e4c6c27b63474ae19181256abd
Caio Vitor <caiocastro460@gmail.com>
2026-08-29 11:21:51 -0300

    refactor: replace Spring Security User with CustomUserDetails

--- src/main/java/dev/caiovitor/eventticketing/security/UserDetailsServiceImpl.java
+++ src/main/java/dev/caiovitor/eventticketing/security/UserDetailsServiceImpl.java
@@ -1,19 +1,15 @@
 package dev.caiovitor.eventticketing.security;
 
 
-import dev.caiovitor.eventticketing.entity.Role;
 import dev.caiovitor.eventticketing.entity.User;
 import dev.caiovitor.eventticketing.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.Set;
-import java.util.stream.Collectors;
+
 
 @Service
 @RequiredArgsConstructor
@@ -21,21 +17,13 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
           User user = userRepository.findByEmailWithRoles(username)
                   .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
-        return new org.springframework.security.core.userdetails
-                .User(user.getEmail(),user.getPassword(),mapRolesToAuthority(user.getRoles()));
+          return new CustomUserDetails(user);
     }
 
-    private Collection<SimpleGrantedAuthority> mapRolesToAuthority(Set<Role> roles){
-        return roles.stream()
-                .map(role-> new SimpleGrantedAuthority(role.getName().toString()))
-                .collect(Collectors.toSet());
-    }
 }
