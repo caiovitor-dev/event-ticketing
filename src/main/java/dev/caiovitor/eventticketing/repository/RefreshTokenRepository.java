e0410aa535a47282b1d6377b2eece2e504215932
Caio Vitor <caiocastro460@gmail.com>
2026-09-03 10:06:10 -0300

    feat: add method to retrieve refresh token

--- src/main/java/dev/caiovitor/eventticketing/repository/RefreshTokenRepository.java
+++ src/main/java/dev/caiovitor/eventticketing/repository/RefreshTokenRepository.java
@@ -3,7 +3,10 @@ package dev.caiovitor.eventticketing.repository;
 import dev.caiovitor.eventticketing.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+
+    Optional<RefreshToken> findByToken(String token);
 }
