ace509a4ae163f425816d9bea5f8e0b5a6c61ae4
Caio Vitor <caiocastro460@gmail.com>
2026-09-04 09:58:04 -0300

    feat: add RefreshTokenRequestDTO

--- src/main/java/dev/caiovitor/eventticketing/dto/RefreshTokenRequestDTO.java
+++ src/main/java/dev/caiovitor/eventticketing/dto/RefreshTokenRequestDTO.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 
 import java.util.UUID;
 
-public record RefreshTokenRequest(
+public record RefreshTokenRequestDTO(
         @NotBlank(message = "token cannot be blank.") UUID refreshToken
 )
 {
