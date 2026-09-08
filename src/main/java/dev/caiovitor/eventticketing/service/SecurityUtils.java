ff3d38588d1c264eaa96046ad50a5976e39605a9
Caio Vitor <caiocastro460@gmail.com>
2026-09-08 09:19:22 -0300

    feat: add SecurityUtils to retrieve the logged-in user

--- src/main/java/dev/caiovitor/eventticketing/service/SecurityUtils.java
+++ src/main/java/dev/caiovitor/eventticketing/service/SecurityUtils.java
@@ -1,4 +1,22 @@
 package dev.caiovitor.eventticketing.service;
 
+import dev.caiovitor.eventticketing.entity.User;
+import dev.caiovitor.eventticketing.security.CustomUserDetails;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+
+@Component
 public class SecurityUtils {
+
+    public User getLoggedUser(){
+
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if(principal instanceof CustomUserDetails customUserDetails){
+           return customUserDetails.getUser();
+        }
+
+        return null;
+    }
 }
