49a70f78bff9dcfa87bd0a6a813b6e4eaf89eccb
Caio Vitor <caiocastro460@gmail.com>
2026-09-11 15:10:36 -0300

    feat: add method to revoke user tokens

--- src/main/java/dev/caiovitor/eventticketing/repository/RefreshTokenRepository.java
+++ src/main/java/dev/caiovitor/eventticketing/repository/RefreshTokenRepository.java
@@ -2,11 +2,23 @@ package dev.caiovitor.eventticketing.repository;
 
 import dev.caiovitor.eventticketing.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
 
-    Optional<RefreshToken> findByToken(String token);
+    public Optional<RefreshToken> findByToken(String token);
+
+    @Modifying
+    @Query("""
+           UPDATE RefreshToken u 
+           SET u.revokedAt = CURRENT_TIMESTAMP 
+           WHERE u.revokedAt IS NULL
+           AND u.user.id =:id
+         """)
+    public void revokeUserTokens(@Param("id")UUID id);
+
 }
